cbb8ae7f65c4331990dd9b55e7776b2a56fd43db
unknown <Benjamin.Roesner@HUNB10.pnlrk.local>
2015-08-24 08:02:12 +0200

    temporarily removed button from addin. Seems that extension and button in same addin does not work.

--- EnProTo_addin/Install/EnProTo_addin_addin_bac.py
+++ EnProTo_addin/Install/EnProTo_addin_addin_bac.py
@@ -35,3 +35,19 @@ class ChangeBrowsePath(object):
 		#write LastSave
 		_winreg.SetValueEx(registrykey,"LastSaveToLocation",0,_winreg.REG_SZ,lastsave)
 		_winreg.CloseKey(registrykey)
+		
+		
+
+
+class OpenPathForSelectedLayer(object):
+    """Implementation for OpenPathForSelectedLayer.button (Button)"""
+    def __init__(self):
+        self.enabled = True
+        self.checked = False
+    def onClick(self):
+	    mxd = arcpy.mapping.MapDocument("current")
+        toclayer = pythonaddins.GetSelectedTOCLayerOrDataFrame()
+        desc = arcpy.Describe(toclayer)
+        path = desc.path
+       
+        subprocess.Popen('explorer "{0}"'.format(path))
