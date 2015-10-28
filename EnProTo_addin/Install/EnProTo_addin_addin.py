a907fbecfc12c166633dc637b424598ff49f92bb
unknown <Benjamin.Roesner@HUNB10.pnlrk.local>
2015-10-28 11:45:18 +0100

    fixed bug in OpenPathForSelectedLayer Button

--- EnProTo_addin/Install/EnProTo_addin_addin.py
+++ EnProTo_addin/Install/EnProTo_addin_addin.py
@@ -37,6 +37,21 @@ class ChangeBrowsePath(object):
 		_winreg.CloseKey(registrykey)
         
         
+class OpenPathForSelectedLayer(object):
+    """Implementation for OpenPathForSelectedLayer.button (Button)"""
+    def __init__(self):
+        self.enabled = True
+        self.checked = False
+    def onClick(self):
+        mxd = arcpy.mapping.MapDocument("CURRENT")
+        toclayer = pythonaddins.GetSelectedTOCLayerOrDataFrame()
+        desc = arcpy.Describe(toclayer)
+        path = desc.path
+       
+        subprocess.Popen('explorer "{0}"'.format(path))
+        pass
+        
+        
 class FindDefinitionQuerys(object):
     """Implementation for FindDefinitionQuerys.button (Button)"""
     def __init__(self):
