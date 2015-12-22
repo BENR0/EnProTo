e2387f788b42d9429939dca01b33b038d47799c9
Ro@gluon <mail2benr@gmail.com>
2015-12-22 20:06:02 +0100

    - upgraded adddescriptiontofeatures.py

--- EnProTo.pyt
+++ EnProTo.pyt
@@ -25,5 +25,5 @@ class Toolbox(object):
 
         #List of tool classes associated with this toolbox
         #self.tools = [AddDescriptionToFeatures,MultiBufferSpeciesExport,PlankopfWizard,GroupAnnosByPF,Legen_WaitForIt_Dary,PolylineToPolygon,KartenserieToPng]
-        self.tools = [AddDescriptionToFeatures,MultiBufferSpeciesExport,KartenserieToPng,BatchConvertPDFToTif,PlankopfWizard,MultiPurposeBufferTool,BatchAddFieldMerge]
+        self.tools = [AddDescriptionToFeatures,KartenserieToPng,BatchConvertPDFToTif,MultiPurposeBufferTool,BatchAddFieldMerge]
 
--- tools/adddescriptiontofeatures.py
+++ tools/adddescriptiontofeatures.py
@@ -9,93 +9,132 @@ class AddDescriptionToFeatures(object):
         self.canRunInBackground = False
 
     def getParameterInfo(self):
-    #Input feature layer
-		in_features = arcpy.Parameter(
-			displayName="Input Feature Layer",
-			datatype="GPFeatureLayer",
-			name="in_features",
-			parameterType="Required",
-			direction="Input")
-			
-		in_features.filter.list = ["Point","Line","Polygon"]
-		
-		#Field to use for description
-		description_field = arcpy.Parameter(
-			displayName="Attribute Table Field to be used as Description",
-			name="description_field",
-			datatype="Field",
-			parameterType="Required",
-			direction="Input")
-			
-		description_field.parameterDependencies = [in_features.name]
-			
-		#Field to use for sorting to unique values
-		sorting_field = arcpy.Parameter(
-			displayName="Attribute Table Field to be used for sorting",
-			name="sorting_field",
-			datatype="Field",
-			parameterType="Required",
-			direction="Input")
-		
-		sorting_field.parameterDependencies = [in_features.name]
+       #Input feature layer
+        in_features = arcpy.Parameter(
+                displayName="Input feature layer",
+                datatype="GPFeatureLayer",
+                name="in_features",
+                parameterType="Required",
+                direction="Input")
+                
+        in_features.filter.list = ["Point","Line","Polygon"]
         
         #Field to use for sorting to unique values
-		selected_fields = arcpy.Parameter(
-			displayName="Selected fields",
-			name="selected_fields",
-			datatype="GPValueTable",
-			parameterType="Optional",
-			direction="Input")
-            
-        #selected_fields.columns = (["GPString", "Field"])
-	
-		parameters = [in_features,description_field,sorting_field, selected_fields]
+        sorting_field = arcpy.Parameter(
+                displayName="Attribute table field to be used for sorting",
+                name="sorting_field",
+                datatype="Field",
+                parameterType="Required",
+                direction="Input")
         
-		return parameters
+        #sorting_field.parameterDependencies = [in_features.name]
+
+        #Fields to use for description
+        description_fields = arcpy.Parameter(
+                displayName="Attribute table fields to be used as description",
+                name="description_fields",
+                datatype="GPValueTable",
+                parameterType="Required",
+                direction="Input")
+        
+        description_fields.parameterDependencies = [in_features.name]
+        description_fields.columns = ([["Field", "Field"], ["GPBoolean", "Bold"], ["GPBoolean",
+            "Italics"], ["GPBoolean", "Underlined"], ["GPBoolean", "Newline"]])
+                
+        description_fields.filters[1].type = "ValueList"
+        description_fields.filters[1].list = ["True", "False"]
+        description_fields.filters[2].type = "ValueList"
+        description_fields.filters[2].list = ["True", "False"]
+        description_fields.filters[3].type = "ValueList"
+        description_fields.filters[3].list = ["True", "False"]
+        description_fields.filters[4].type = "ValueList"
+        description_fields.filters[4].list = ["True", "False"]
+        
+	
+        parameters = [in_features,sorting_field, description_fields]
+
+        return parameters
 
     def isLicensed(self):
         """Set whether tool is licensed to execute."""
         return True
 
     def updateParameters(self, parameters):
-
+        #mxd = arcpy.mapping.MapDocument("current")
+        lyr = parameters[0].valueAsText
+        if parameters[0].value:
+            if parameters[0].value.symbologyType == "UNIQUE_VALUES":
+                parameters[1].value = parameters[0].value.symbology.valueField
+            else:
+                parameters[1].value = ""
         return
 
     def updateMessages(self, parameters):
-        
+        if parameters[0].value:
+            if parameters[0].value.symbologyType != "UNIQUE_VALUES":
+                parameters[0].setErrorMessage("This layers symbology is not of type \"unique values\".")
         return
 
     def execute(self, parameters, messages):
-        from itertools import chain
 		
         features = parameters[0].valueAsText
-        s_field = parameters[2].valueAsText
-        d_field = parameters[1].valueAsText
+        keyfield = parameters[1].valueAsText
+        d_field = parameters[2].value
+        #valuetable column names
+        vtcolumns = ["Field", "Bold", "Italics", "Underlined", "Newline"]
+        #init empty dictionary for valuetable
+        dict_valuetable = {}
+        #read valuetable to dictionary
+        for row in d_field:
+            dict_valuetable[str(row[0])] = dict(zip(vtcolumns, row))
 
-        ##snippets from standalone script
+        arcpy.AddMessage(dict_valuetable)
+
         mxd = arcpy.mapping.MapDocument("current")
-        lyr = arcpy.mapping.ListLayers(mxd,features)[0]
-
-	def unique_values(table, field1,field2):
-		with arcpy.da.SearchCursor(table, [field1,field2]) as cursor:
-			return sorted({(row[0],row[1]) for row in cursor})
-		
-		
-        desc_list = unique_values(lyr,s_field,d_field)
-    	desc_list = map(list,desc_list)
-    	desc_list = list(chain.from_iterable(desc_list))
-
-        #print desc_list 
-
-    	if lyr.symbologyType == "UNIQUE_VALUES":
-            nvalues = len(lyr.symbology.classValues)
-            if len(desc_list[1::2]) != nvalues:
-                arcpy.AddMessage("Anzahl der Beschreibungen " + str(nvalues) + " entspricht nicht der Anzahl der Klassen " + str(len(desc_list[1::2])))
-                #arcpy.AddMessage(print("\n".join([str(element) for element in desc_list[1::2]])))
-                arcpy.AddMessage(str(desc_list[1::2]))
+        lyr = arcpy.mapping.ListLayers(mxd, features)[0]
+        
+        #function to read attribute table to dictionary based on a key field
+        def make_attribute_dict(fc, key_field, attr_list=['*']):
+            attdict = {}
+            fc_field_objects = arcpy.ListFields(fc)
+            fc_fields = [field.name for field in fc_field_objects if field.type != 'Geometry']
+            if attr_list == ['*']:
+                valid_fields = fc_fields
             else:
-                #lyr.symbology.valueField = s_field
-                lyr.symbology.classDescriptions = desc_list[1::2]
-                lyr.symbology.showOtherValues = False
+                valid_fields = [field for field in attr_list if field in fc_fields]
+            #Ensure that key_field is always the first field in the field list
+            cursor_fields = [key_field] + list(set(valid_fields) - set([key_field]))
+            with arcpy.da.SearchCursor(fc, cursor_fields) as cursor:
+                for row in set(cursor):
+                    attdict[row[0]] = dict(zip(cursor.fields,row))
+            return attdict
+
+        #create dictionary from table with selected fields
+        attrdict = make_attribute_dict(features, keyfield, dict_valuetable.keys())
+        #init empty description list
+        class_descriptions = []
+
+        for i in lyr.symbology.classValues:
+            tmp = ""
+            for f in dict_valuetable.keys():
+                otag = ""
+                ctag = ""
+                if dict_valuetable[f]["Newline"]:
+                    otag = otag + chr(13) + chr(10) #"\r\n"
+                elif dict_valuetable[f]["Bold"]:
+                    otag = otag + "<BOL>"
+                    ctag = "</BOL>" + ctag 
+                elif dict_valuetable[f]["Italics"]:
+                    otag = otag + "<ITA>"
+                    ctag = "</ITA>" + ctag
+                elif dict_valuetable[f]["Underline"]:
+                    otag = otag + "<UND>"
+                    ctag = "</UND>" + ctag
+
+                tmp = tmp + otag + attrdict[i][f] + ctag
+            class_descriptions.append(tmp)
+
+        lyr.symbology.classDescriptions = class_descriptions
+        lyr.symbology.showOtherValues = False
 
         return
