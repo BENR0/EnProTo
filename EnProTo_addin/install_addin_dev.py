865b5c63f22f4661fb8d65b29b0273ee6ca53b64
unknown <Benjamin.Roesner@HUNB10.pnlrk.local>
2016-06-08 16:07:46 +0200

    - update to install script to use user name

--- EnProTo_addin/install_addin_dev.py
+++ EnProTo_addin/install_addin_dev.py
@@ -2,25 +2,32 @@
 import shutil
 import subprocess
 import os
+import traceback
 
-addindir = r"C:\Users\Benjamin.Roesner\Documents\ArcGIS\AddIns\Desktop10.1\{de2881e9-b5ef-4dd1-bd2b-8c2cbe8b49a7}\enproto_addin.esriaddin"
-desktop = r"C:\Users\ro\Desktop\EnProTo_addin"
-shareddir = r"E:\toolboxes\EnProTo\EnProTo_addin"
-addinsourcedir = r"L:\Ablage_Mitarbeiter\Benjamin\dev\EnProTo_dev\EnProTo_addin\EnProTo_addin.esriaddin"
+try:
+    user = os.environ.get("USERNAME")
 
-#copy folder from shared dir to desktop of virtial machine
-#if os.path.exists(desktop):
-#    shutil.rmtree(desktop)
-#shutil.copytree(shareddir, desktop)
-user = os.environ.get("USERNAME")
-#cat together all toolbar scripts
-cat = r"L:\Ablage_Mitarbeiter\Benjamin\dev\EnProTo_dev\EnProTo_addin\cat.py"
-subprocess.call(["python", cat])
-#run make addin script
-test = r"L:\Ablage_Mitarbeiter\Benjamin\dev\EnProTo_dev\EnProTo_addin\makeaddin.py" # os.path.join(desktop, "makeaddin.py")
-subprocess.call(["python", test])
-if os.path.exists(addindir):
-    os.remove(addindir)
-shutil.copy(addinsourcedir, addindir)
+    addindir = os.path.join("C:\Users", user, "Documents\ArcGIS\AddIns\Desktop10.1\{de2881e9-b5ef-4dd1-bd2b-8c2cbe8b49a7}\enproto_addin.esriaddin")
+    desktop = r"C:\Users\ro\Desktop\EnProTo_addin"
+    shareddir = r"E:\toolboxes\EnProTo\EnProTo_addin"
+    addinsourcedir = r"L:\Ablage_Mitarbeiter\Benjamin\dev\EnProTo_dev\EnProTo_addin\EnProTo_addin.esriaddin"
 
-#subprocess.Popen(["ArcMap.exe"])
+    #copy folder from shared dir to desktop of virtial machine
+    #if os.path.exists(desktop):
+    #    shutil.rmtree(desktop)
+    #shutil.copytree(shareddir, desktop)
+    #cat together all toolbar scripts
+    cat = r"L:\Ablage_Mitarbeiter\Benjamin\dev\EnProTo_dev\EnProTo_addin\cat.py"
+    subprocess.call(["python", cat])
+    #run make addin script
+    test = r"L:\Ablage_Mitarbeiter\Benjamin\dev\EnProTo_dev\EnProTo_addin\makeaddin.py" # os.path.join(desktop, "makeaddin.py")
+    subprocess.call(["python", test])
+    if os.path.exists(addindir):
+        os.remove(addindir)
+    shutil.copy(addinsourcedir, addindir)
+
+    #subprocess.Popen(["ArcMap.exe"])
+except:
+    traceback.print_exc()
+
+raw_input("Press Enter to close") # Python 2
