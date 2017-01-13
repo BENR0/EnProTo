f6f59e41644c9c7e457f23ee411bbcd58e926c8b
Ro@gluon <mail2benr@gmail.com>
2017-01-13 16:56:52 +0100

    - finalized dynamic bird table

--- EnProTo_addin/cat.py
+++ EnProTo_addin/cat.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import glob
 
-files = glob.glob("mod/*.py")
+files = sorted(glob.glob("mod/*.py"))
 
 with open("Install/EnProTo_addin_addin.py", "wb") as outfile:
     for f in files:
--- EnProTo_addin/mod/15_dynamische_vogel_tab.py
+++ EnProTo_addin/mod/15_dynamische_vogel_tab.py
@@ -1,46 +1,40 @@
-class DynVogelTab(object):
-    """Implementation for DynVogelTab.button (Button)"""
+class DynBirdTab(object):
+    """Implementation for DynBirdTab.button (Button)"""
     def __init__(self):
         self.enabled = True
         self.checked = False
-    @property
     def onClick(self):
         import arcpy
         import pythonaddins
         #init vars
         #positioncounter
-        iPosition = [10.0, 25.0]
+        #iPosition = [10.0, 25.0]
         #distances of and between elements
-        sigwidth = 1.0
         txtElemWidth = 6.0
-        sigheight = 0.5
-        sig2group = 0.5
-        group2item = 0.6
-        item2item = 0.1
-        group2group = 0.8
         #font characteristics
         fontName = "Arial"
         headingFontSize = 14.0
         itemFontSize = 12.0
 
         #Get/set information about the table
-        rowHeight = 0.2
+        rowHeight = 0.4
         fieldNames = ["Abk", "Name_dt", "Name_wiss"]
-        colWidth = 1.5
-
-        #start coords for elements
-        upperX = 1.0
-        upperY = 5.0
+        colWidth = 4.0
 
 
         #Add initial text element
-        AddTextElement(position = (0.0, 0.0), txtstring = "txtElemTemplate", tag = "txtElemTemplate",
-                       txtsize = itemFontSize)
+        AddTextElement(position = (0.0, 0.0), txtstring = "txtElemTemplate",
+                        tag = "txtElemTemplate", txtsize = itemFontSize)
         #get created text element
         mxd = arcpy.mapping.MapDocument("current")
         tableText = arcpy.mapping.ListLayoutElements(mxd, "TEXT_ELEMENT")[0]
 
         toclayer = pythonaddins.GetSelectedTOCLayerOrDataFrame()
+
+        #start coords for elements
+        pageWidth = mxd.pageSize.width
+        upperX = pageWidth + 3.0
+        upperY = 25.0
 
         #get data from attribute table and store rows as tuples in list
         tabledata = []
@@ -55,9 +49,6 @@ class DynVogelTab(object):
         #or inplace
         tabledata.sort(key=lambda tup: tup[1])
 
-        #width counter
-        accumWidth = colWidth
-
         #Place starting text element
         tableText.elementPositionX = upperX + 0.05 #slight offset
         tableText.elementPositionY = upperY
@@ -65,20 +56,27 @@ class DynVogelTab(object):
         #Create text elements based on values from the table
         y = upperY - rowHeight
         for row in tabledata:
-          x = upperX + 0.05 #slight offset
-          try:
-            for field in row:
-              newCellTxt = tableText.clone("_clone")
-              newCellTxt.text = field
-              newCellTxt.elementPositionX = x
-              newCellTxt.elementPositionY = y
-              accumWidth = accumWidth + colWidth
-              x = x + colWidth
-            y = y - rowHeight
-          except:
-            print("Invalid value assignment")
+            x = upperX + 0.05 #slight offset
+            try:
+                y = y - rowHeight
+                for field in row:
+                    newCellTxt = tableText.clone("_clone")
+                    #make last field with latin name italic
+                    if row.index(field) == 2:
+                        field = "<ITA>" + field + "</ITA>"
+                    newCellTxt.text = field
+                    newCellTxt.elementPositionX = x
+                    newCellTxt.elementPositionY = y
+                    #make first column width smaller
+                    if row.index(field) == 0:
+                        curColWidth = 1.0
+                    else:
+                        curColWidth = colWidth
+                    x = x + curColWidth
+            except:
+                print("Invalid value assignment")
 
-        #for elm in lyt.listElements(wildcard="_clone"):
-         # elm.delete()
+        for elm in arcpy.mapping.ListLayoutElements(mxd, wildcard="txtElemTemplate"):
+            elm.delete()
 
         pass
