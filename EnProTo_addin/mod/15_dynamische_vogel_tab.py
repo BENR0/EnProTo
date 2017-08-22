f26dd0df43f9818e846e9e6675a3532d59b7bb1b
Ro <mail2benr@gmail.com>
2017-08-22 15:20:37 +0200

    Fix typo

--- EnProTo_addin/mod/15_dynamische_vogel_tab.py
+++ EnProTo_addin/mod/15_dynamische_vogel_tab.py
@@ -9,7 +9,7 @@ class DynBirdTab(object):
         import logging
 
         #usage logging
-        log_use(str(self.__class__.__name__)
+        log_use(str(self.__class__.__name__))
 
         #init vars
         #positioncounter
